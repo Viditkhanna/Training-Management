4e87314175c7723a2188b56e6269387514a5412d
Vidit <viditkhanaa@gmail.com>
2025-05-07 16:50:10 +0200

    Add loading state cases in views

--- lib/feature/event_management/view/event_management_view.dart
+++ lib/feature/event_management/view/event_management_view.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:training_management/feature/event_management/model/event.dart';
+import 'package:training_management/feature/event_management/notifier/event_management_notifier.dart';
+import 'package:training_management/feature/event_management/notifier/event_management_state.dart';
 
-class EventManagementView extends StatelessWidget {
+class EventManagementView extends ConsumerWidget {
   const EventManagementView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final event = ref.watch(eventManagementNotifierProvider);
+
+    return Scaffold(
+      body: switch (event.loadingState) {
+        Initial<Event>() => SizedBox.shrink(),
+        Loading<Event>() => Center(child: CircularProgressIndicator()),
+        Success<Event>(event: Event event) => ListView(children: []),
+        Failure<Event>(exception: Exception exception) => Center(
+          child: Text(exception.toString()),
+        ),
+      },
+    );
   }
 }
