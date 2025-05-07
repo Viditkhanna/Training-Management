4686295d58d36db37beb3c7b46e34f3a4507264f
Vidit <viditkhanaa@gmail.com>
2025-05-07 17:30:45 +0200

    Update theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'feature/event_management/view/event_management_view.dart';
 
 void main() {
@@ -10,19 +9,42 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static const font = 'SourceSansPro';
+
   @override
   Widget build(BuildContext context) {
     return ProviderScope(
       child: MaterialApp(
         title: 'Event',
         theme: ThemeData(
-          fontFamily: 'SourceSansPro',
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          fontFamily: font,
+          primaryColor: Colors.black,
+          colorScheme: ColorScheme.light(primary: Colors.black),
+          tabBarTheme: TabBarTheme(
+            unselectedLabelColor: Colors.grey,
+            dividerColor: Colors.grey,
+            unselectedLabelStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontFamily: font,
+              fontSize: 16,
+            ),
+            labelStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontFamily: font,
+              fontSize: 16,
+            ),
+            dividerHeight: 0.2,
+            indicatorSize: TabBarIndicatorSize.tab,
+            indicator: UnderlineTabIndicator(
+              borderSide: BorderSide(color: Colors.black, width: 1.5),
+            ),
+          ),
           appBarTheme: AppBarTheme(
             titleTextStyle: TextStyle(
               fontSize: 16,
+              fontWeight: FontWeight.bold,
               color: Colors.black,
-              fontFamily: 'SourceSansPro',
+              fontFamily: font,
             ),
           ),
         ),
