806657523b821664adf59920e95562758d64bf59
Vidit <viditkhanaa@gmail.com>
2025-05-07 15:22:18 +0200

    Add EventManagement view class

---
+++ lib/feature/event_management/view/event_management.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EventManagement extends StatelessWidget {
+  const EventManagement({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
