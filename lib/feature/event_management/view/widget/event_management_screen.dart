37a264bf9fba4a6b05450a67b9cf421ccf544ccc
Vidit <viditkhanaa@gmail.com>
2025-05-07 19:14:08 +0200

    Add ExpansionTile

--- lib/feature/event_management/view/widget/event_management_screen.dart
+++ lib/feature/event_management/view/widget/event_management_screen.dart
@@ -80,20 +80,59 @@ class ResourceTileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Container(
-        height: 40,
-        width: 40,
-        padding: EdgeInsets.all(4),
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          border: Border.all(color: Colors.grey, strokeAlign: 5),
-          image: DecorationImage(image: NetworkImage(resource.photo)),
-        ),
+    if (resource.certificates.isEmpty) {
+      return ListTile(
+        leading: _ResourceImage(imageUrl: resource.photo),
+        subtitle: Text(resource.userId),
+        title: Text('${resource.firstName} ${resource.name}'),
+        contentPadding: EdgeInsets.zero.copyWith(left: 4),
+      );
+    } else {
+      return ExpansionTile(
+        title: Text('${resource.firstName} ${resource.name}'),
+        childrenPadding: EdgeInsets.zero,
+        tilePadding: EdgeInsets.zero.copyWith(left: 4),
+        shape: Border(),
+        leading: _ResourceImage(imageUrl: resource.photo),
+        subtitle: Text(resource.userId),
+        children: [
+          Container(
+            color: Colors.blue,
+            padding: EdgeInsets.all(4),
+            width: MediaQuery.of(context).size.width,
+            child: Text('Certificates', style: TextStyle(color: Colors.white)),
+          ),
+          SizedBox(height: 8),
+          ...resource.certificates
+              .map(
+                (certificate) => Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(certificate),
+                ),
+              )
+              .expand((e) => [e, Divider(color: Colors.grey, thickness: 0.4)])
+              .toList()
+            ..removeLast(),
+        ],
+      );
+    }
+  }
+}
+
+class _ResourceImage extends StatelessWidget {
+  const _ResourceImage({required this.imageUrl});
+  final String imageUrl;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      width: 40,
+      padding: EdgeInsets.all(4),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(color: Colors.grey, strokeAlign: 5),
+        image: DecorationImage(image: NetworkImage(imageUrl)),
       ),
-      subtitle: Text(resource.userId),
-      title: Text('${resource.firstName} ${resource.name}'),
-      contentPadding: EdgeInsets.zero.copyWith(left: 4),
     );
   }
 }
